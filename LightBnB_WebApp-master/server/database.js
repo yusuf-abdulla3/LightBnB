f6be241e1a4b2258787f41f84a10b4229bfe0055
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-20 03:09:01 +0000

    Completed reservations queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -98,8 +98,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+
+  const queryString = `
+    SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+    FROM reservations 
+    JOIN properties ON properties.id = reservations.property_id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1 AND reservations.end_date < Now()::date
+    GROUP BY properties.id, reservations.id;
+    LIMIT $2;
+  `;
+  const values = [guest_id, limit];
+  return pool
+  .query(queryString, values)
+    .then(res => {
+      return res.rows[0];
+    })
+    .catch(err => {
+      return console.log('query error:', err);
+    })
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
